7a55eb8bdfcb435756d26ef7b0306640ad8c7920
ohko <ohko@qq.com>
2020-02-15 22:45:29 +0800

    修正forward长连接问题

--- forward/main.go
+++ forward/main.go
@@ -40,7 +40,21 @@ func proxy(conn net.Conn) {
 		return
 	}
 
-	go io.Copy(conn, local)
-	io.Copy(local, conn)
+	ch := make(chan int, 1)
+	go func() {
+		io.Copy(local, conn)
+		select {
+		case ch <- 1:
+		default:
+		}
+	}()
+	go func() {
+		io.Copy(conn, local)
+		select {
+		case ch <- 1:
+		default:
+		}
+	}()
+	<-ch
 	local.Close()
 }
